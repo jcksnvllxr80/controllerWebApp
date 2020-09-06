48fa966ae70b53d1b99810a5c4f026177be19f34
Aaron Watkins <ac.watkins80@gmail.com>
2020-09-05 23:43:35 -0400

    read pedal config from controller into dict

--- js/http_requests.js
+++ js/http_requests.js
@@ -1,16 +1,23 @@
 const url='http://midi-controller:8081'
+var midi_pedal_config_dict = {};
 
 function getResponse(requestUrl) {
   $.getJSON(requestUrl, function(result) {
     console.log(result);
+    return result;
   });
 }
 
-// get sets
-getResponse(`${url}/sets`);
+// get midi controller's sets
+var sets = getResponse(`${url}/sets`);
+// get midi controller's songs
+var songs = getResponse(`${url}/songs`);
+// get midi controller's pedals
+var pedals = getResponse(`${url}/pedals`);
 
-// get songs
-getResponse(`${url}/songs`);
+pedals.array.forEach(midi_pedal => {
+  // get pedal's configuration and store into a dictinary
+  midi_pedal_config_dict[midi_pedal] = getResponse(`${url}/pedal/${midi_pedal}`);
+});
 
-// get pedals
-getResponse(`${url}/pedals`);
+console.log(midi_pedal_config_dict.getJSON)
