440b310db79a4a6a61aaeb153083d158403d0e79
Aaron Watkins <ac.watkins80@gmail.com>
2020-09-19 14:32:21 -0400

    compare numbers not strings

--- js/http_requests.js
+++ js/http_requests.js
@@ -738,7 +738,7 @@ function setSongTempo() {
   var songName = editSongTempoSelect.parentNode.value;
   var songJson = getJsonForSongDotYaml(songName);
   var currentSongTempo = songJson.tempo;
-  if (currentSongTempo == null || currentSongTempo.localeCompare(editSongTempoSelect.value) != 0) {
+  if (currentSongTempo == null || currentSongTempo != editSongTempoSelect.value) {
     console.debug(`Setting song tempo to, \'${editSongTempoSelect.value}\', for song, \'${songName}\'.`);
     songJson.tempo = Number(editSongTempoSelect.value);
   } else {
